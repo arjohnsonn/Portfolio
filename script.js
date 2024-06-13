446228dca46e9dfb4ac4ffbd133547aedd8d170b
Aiden Johnson <arjohnsonn12@gmail.com>
2024-06-13 03:06:03 -0500

    Revamp

--- script.js
+++ script.js
@@ -1,22 +1,139 @@
-const express = require("express");
-const fetch = require("node-fetch");
+var canvas = document.getElementById("canvas"),
+  ctx = canvas.getContext("2d");
 
-const app = express();
-const PORT = 3000;
+const starSpeed = 65;
+var stars = [], // Array that contains the stars
+  FPS = 144, // Frames per second
+  x = 230, // Number of stars
+  mouse = {
+    x: 0,
+    y: 0,
+  }; // mouse location
 
-app.get("/games", async (req, res) => {
-  try {
-    const response = await fetch(
-      "https://games.roblox.com/v1/games?universeIds=4221645607"
-    );
-    const data = await response.json();
-    res.json(data);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    res.status(500).send("Internal Server Error");
+// Increase canvas resolution and scale context
+function initCanvas() {
+  canvas.width = window.innerWidth * 3;
+  canvas.height = window.innerHeight * 3;
+  canvas.style.width = window.innerWidth + "px";
+  canvas.style.height = window.innerHeight + "px";
+  ctx.scale(3, 3);
+
+  stars = [];
+
+  // Push stars to array
+  for (var i = 0; i < x; i++) {
+    stars.push({
+      x: Math.random() * canvas.width,
+      y: Math.random() * canvas.height,
+      radius: Math.random() * 1 + 1,
+      vx: Math.floor(Math.random() * starSpeed),
+      vy: Math.floor(Math.random() * starSpeed),
+    });
   }
+}
+
+const typewriterText = document.getElementById("typewriter-text");
+
+function updateText() {
+  // handle landing page text resizing
+  if (window.innerWidth < 750) {
+    typewriterText.style.fontSize = "3.5vw";
+  } else {
+    typewriterText.style.fontSize = "2vw";
+  }
+}
+
+initCanvas();
+updateText();
+addEventListener("resize", () => {
+  initCanvas();
+  updateText();
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// Draw the scene
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw stars
+  ctx.globalCompositeOperation = "source-over";
+  for (var i = 0, x = stars.length; i < x; i++) {
+    var s = stars[i];
+    ctx.fillStyle = "#fff";
+    ctx.beginPath();
+    ctx.arc(s.x / 3, s.y / 3, s.radius, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.fillStyle = "black";
+    ctx.stroke();
+  }
+
+  ctx.beginPath();
+  for (var i = 0, x = stars.length; i < x; i++) {
+    var starI = stars[i];
+    ctx.moveTo(starI.x / 3, starI.y / 3);
+    if (distance(mouse, starI) < 350) ctx.lineTo(mouse.x / 3, mouse.y / 3);
+    for (var j = 0, x = stars.length; j < x; j++) {
+      var starII = stars[j];
+      if (distance(starI, starII) < 150) {
+        ctx.lineTo(starII.x / 3, starII.y / 3);
+      }
+    }
+  }
+  ctx.lineWidth = 0.5;
+  ctx.strokeStyle = "gray";
+  ctx.stroke();
+
+  // Create vignette effect around mouse
+  var gradient = ctx.createRadialGradient(
+    mouse.x / 3,
+    mouse.y / 3,
+    50,
+    mouse.x / 3,
+    mouse.y / 3,
+    350
+  );
+  gradient.addColorStop(0, "rgba(0, 0, 0, 0)");
+  gradient.addColorStop(1, "rgba(0, 0, 0, 1)");
+
+  ctx.globalCompositeOperation = "destination-out";
+  ctx.fillStyle = gradient;
+  ctx.fillRect(0, 0, canvas.width / 3, canvas.height / 3);
+}
+
+function distance(point1, point2) {
+  var xs = 0;
+  var ys = 0;
+
+  xs = point2.x - point1.x;
+  xs = xs * xs;
+
+  ys = point2.y - point1.y;
+  ys = ys * ys;
+
+  return Math.sqrt(xs + ys);
+}
+
+// Update star locations
+function update() {
+  for (var i = 0, x = stars.length; i < x; i++) {
+    var s = stars[i];
+    s.x += s.vx / FPS;
+    s.y += s.vy / FPS;
+
+    if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
+    if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
+  }
+}
+
+canvas.addEventListener("mousemove", function (e) {
+  mouse.x = e.clientX * 3;
+  mouse.y = e.clientY * 3;
 });
+
+// Update and draw
+function tick() {
+  draw();
+  update();
+  requestAnimationFrame(tick);
+}
+
+tick();
