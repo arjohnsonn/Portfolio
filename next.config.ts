0ff497b472f7ec210df1e010b8c13fd9de5110c9
Aiden Johnson <arjohnsonn12@gmail.com>
2025-04-05 17:40:36 -0500

    fix?: CORS

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         // matching all Hosts routes
-        source: "/Hosts/help-me-bevo-quotes.json",
+        source: "/Hosts/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
